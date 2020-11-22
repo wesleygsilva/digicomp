1d6a38bc1178ab4538d0603d300fd5e09b79532e
gomeswesley <gomes.wesley@outlook.com.br>
2020-11-22 12:55:12 -0300

    Commit Inicial

---
+++ js/main.js
@@ -0,0 +1,114 @@
+jQuery(document).ready(function( $ ) {
+
+    // inicializa o wowjs
+  new WOW().init();
+
+
+
+  // Header fixo e volta para botão top 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+      $('#header').addClass('header-fixed');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+      $('#header').removeClass('header-fixed');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+
+  // Mobile Navigation
+  if( $('#nav-menu-container').length ) {
+    var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
+    $mobile_nav.find('> ul').attr({ 'class' : '', 'id' : '' });
+    $('body').append( $mobile_nav );
+    $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
+    $('body').append( '<div id="mobile-body-overly"></div>' );
+    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+
+    $(document).on('click', '.menu-has-children i', function(e){
+      $(this).next().toggleClass('menu-item-active');
+      $(this).nextAll('ul').eq(0).slideToggle();
+      $(this).toggleClass("fa-chevron-up fa-chevron-down");
+    });
+
+    $(document).on('click', '#mobile-nav-toggle', function(e){
+      $('body').toggleClass('mobile-nav-active');
+      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+      $('#mobile-body-overly').toggle();
+    });
+
+    $(document).click(function (e) {
+      var container = $("#mobile-nav, #mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+       if ( $('body').hasClass('mobile-nav-active') ) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+      }
+    });
+  } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
+    $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
+
+  // scroll suave nas hash links (sections)
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if( $('#header').length ) {
+          top_space = $('#header').outerHeight();
+
+          if( ! $('#header').hasClass('header-fixed') ) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ( $(this).parents('.nav-menu').length ) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ( $('body').hasClass('mobile-nav-active') ) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  
+
+
+
+// custom code
+
+// Mascara telefone
+var SPMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+spOptions = {
+  onKeyPress: function(val, e, field, options) {
+    field.mask(SPMaskBehavior.apply({}, arguments), options);
+  }
+};
+
+$('#sp_celphones').mask(SPMaskBehavior, spOptions);
+
+
+
+});
